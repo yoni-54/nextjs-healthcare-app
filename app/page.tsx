21670cd655ae61875fe07a7726993a6ad5c650b7
yoni-54 <yonidaniel54@gmail.com>
2025-02-18 13:08:34 +0300

    Updated the register page

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
-      /*todo:otp */
-      <section className="remove-scrollbar container my-auto">
+      {/*todo:otp*/}  
+        <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
           src="/assets/icons/logo-full.svg"
