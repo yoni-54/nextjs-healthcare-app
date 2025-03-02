2a90175ed6e24b1c1fdd9c31279351a4ce7c17f9
yoni-54 <yonidaniel54@gmail.com>
2025-03-02 15:19:14 +0300

    Added  admin page and implimented sentry and messaging

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -14,15 +14,18 @@ import { createUser } from "@/lib/actions/patient.actions"
 import { FormFieldType } from "./PatientForm"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
 import { Doctors } from "@/constants"
+import { Appointment } from "@/types/appwrite.types"
 
 const AppointmentForm = ({
-    userId, patientId, type        
+    userId, patientId, type, appointment, setOpen     
     }: {
         userId: string;
         patientId: string;
         type: "create" | "cancel" | "schedule";
+        appointment?: Appointment;
+        setOpen: (open: boolean) => void;
     }
 ) => {
   const router = useRouter();
@@ -33,11 +36,11 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-     primaryPhysician: "",
-     schedule: new Date(),
-     reason: "",
-     note: "",
-     cancellationReason: "",
+     primaryPhysician: appointment ? appointment.primaryPhysician : '',
+     schedule: appointment ? new Date(appointment?.schedule): new Date(Date.now()),
+     reason: appointment ? appointment.reason: '',
+     note: appointment?.note || '',
+     cancellationReason: appointment?.cancellationReason || '',
     },
   })
 
@@ -74,6 +77,24 @@ const AppointmentForm = ({
                 form.reset();
                 router.push(`/patients/${userId}/new-appointment/success?appointmentid=${appointment.$id}`)
             }
+        } else {
+          const appointmentToUpdate = {
+            userId,
+            appointmentId: appointment?.$id!,
+            appointment: {
+              primaryPhysician: values?.primaryPhysician,
+              schedule: new Date(values?.schedule),
+              status: status as Status,
+              cancellationReason: values?.cancellationReason,
+            },
+            type
+          }
+          const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+          if(updatedAppointment){
+            setOpen && setOpen(false);
+            form.reset();
+          }
         }
 
       } catch (error) {
@@ -100,10 +121,10 @@ switch (type) {
   return (
 <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1>New Appointment</h1>
+        {type === 'create' && <section className="mb-12 space-y-4">
+          <h1 className="header">New Appointment</h1>
           <p className="text-dark-700">Request a new appointment in 10 seconds</p>
-        </section>     
+        </section>} 
 
         {type !== "cancel" && (
             <>
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://98269b9c23591ec642a3bbec5e8314ac@o4508906790780928.ingest.de.sentry.io/4508906805198928",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
